3c5c6098ead4533d5b43d34f0fe139bebb0a3833
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-10-31 00:48:32 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Chatbot from 'react-chatbot-kit';
+import 'react-chatbot-kit/build/main.css';
 import './App.css';
 
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<Chatbot config={config} actionProvider={ActionProvider} messageParser={MessageParser} />
+			</header>
+		</div>
+	);
 }
 
 export default App;
