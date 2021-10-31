641c3df221adeabf210069802c33781ed452d1a7
Arunabh Ghosh <arunabh.tech@gmail.com>
2021-11-01 01:22:43 +0530

    First commit

---
+++ src/AxleAvatar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import BotAvatar from './icons/bot.svg';
+
+const AxleAvatar = () => {
+	return (
+		<div className='react-chatbot-kit-chat-bot-avatar'>
+			<div className='react-chatbot-kit-chat-bot-avatar-container' style={{ background: 'none' }}>
+				<img alt='BotAvatar' src={BotAvatar} />
+			</div>
+		</div>
+	);
+};
+
+export default AxleAvatar;
